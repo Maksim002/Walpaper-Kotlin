5cc3a08af20737267d12ce48e954b6be6753183a
Maksim002 <inoplanee@mail.ru>
2020-05-06 20:50:26 +0600

    new

--- app/src/main/java/com/example/walpaper_kotlin/PersonalViewHolderPayments.kt
+++ app/src/main/java/com/example/walpaper_kotlin/PersonalViewHolderPayments.kt
@@ -1,12 +1,17 @@
 package com.example.tsj.adapters.pesonal
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.example.walpaper_kotlin.R
 import kotlinx.android.synthetic.main.item_personal.view.*
 
@@ -21,8 +26,29 @@ class PersonalViewHolderPayments(itemView: View) : RecyclerView.ViewHolder(itemV
                 .apply(RequestOptions())
                 .transform(
                     CenterCrop(),
-                    RoundedCorners(10)
-                )
+                    RoundedCorners(10))
+                .listener(object : RequestListener<Drawable?> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any,
+                        target: Target<Drawable?>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        itemView.progress_personal.visibility = View.VISIBLE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any,
+                        target: Target<Drawable?>,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        itemView.progress_personal.visibility = View.GONE
+                        return false
+                    }
+                })
                 .into(imageIcon)
 
 
